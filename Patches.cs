cfde73f3a0fbed2a5f547dfea4aad8c16609bd0c
MMike17 <matthews.r.mike@gmail.com>
2025-04-12 11:43:57 +0200

    Add todos

--- Patches.cs
+++ Patches.cs
@@ -1,27 +1,36 @@
 using HarmonyLib;
 
+// TODO : Load all songs from local folder
+// TODO :	Create local folder
+// TODO : Select which playlist to play
+// TODO :	Race mode only for now
+// TODO :	Detect when we enter race mode
+// TODO :	Change playlist on the fly
+// TODO : Add song titles to 
+// TODO : Add settings to adjust audio sound
+
 namespace Music
 {
-	// Patch model
-	// [HarmonyPatch(typeof(), nameof())]
-	// [HarmonyPatch(typeof(), MethodType.)]
-	// static class type_method_Patch
-	// {
-	// 	static void Prefix()
-	// 	{
-	// 		//
-	// 	}
-	
-	//	this will negate the method
-	//  	static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-	//  	{
-	//      	foreach (var instruction in instructions)
-	//          	yield return new CodeInstruction(OpCodes.Ret);
-	//  	}
+    // Patch model
+    // [HarmonyPatch(typeof(), nameof())]
+    // [HarmonyPatch(typeof(), MethodType.)]
+    // static class type_method_Patch
+    // {
+    // 	static void Prefix()
+    // 	{
+    // 		//
+    // 	}
 
-	// 	static void Postfix()
-	// 	{
-	// 		//
-	// 	}
-	// }
+    //	this will negate the method
+    //  	static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    //  	{
+    //      	foreach (var instruction in instructions)
+    //          	yield return new CodeInstruction(OpCodes.Ret);
+    //  	}
+
+    // 	static void Postfix()
+    // 	{
+    // 		//
+    // 	}
+    // }
 }
